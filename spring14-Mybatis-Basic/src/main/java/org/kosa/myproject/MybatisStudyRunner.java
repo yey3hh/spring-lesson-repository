f516f5bc9c8a2f88d7fb663b76d2093e4ab92222
yey3hh <wateveryehwant@gmail.com>
2025-07-18 14:34:42 +0900

    feat: Mybatis basic crud practice

--- spring14-Mybatis-Basic/src/main/java/org/kosa/myproject/MybatisStudyRunner.java
+++ spring14-Mybatis-Basic/src/main/java/org/kosa/myproject/MybatisStudyRunner.java
@@ -1,5 +1,8 @@
 package org.kosa.myproject;
 
+import java.util.List;
+
+import org.kosa.myproject.domain.Member;
 import org.kosa.myproject.mapper.MemberMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,9 +19,40 @@ public class MybatisStudyRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		int totalCount = memberMapper.getTotalMemberCount();
-		System.out.println("총 회원 수 : " + totalCount);
-
+//		int totalCount = memberMapper.getTotalMemberCount();
+//		System.out.println("총 회원 수 : " + totalCount);
+		
+//		// insert test
+//		Member member = new Member("mybatis", "a", "이강인", "파리");
+//		int registerResult = memberMapper.register(member);
+//		System.out.println("회원 등록 " + registerResult); // 등록된 회원 수 출력
+		
+		// select test
+		// Mapper.xml 에서 parameterType="String" resultType="org.kosa.myproject.domain.Member"
+//		Member member = memberMapper.findMemberById("mybatis");
+//		System.out.println("회원 정보 : " + member); 	// 회원 정보 toString이 출력 : 이강인 정보가 출력
+		
+		// select List test
+//		List<Member> list = memberMapper.findAllMemberList();
+//		for (Member m : list) {
+//			System.out.println(m);
+//		}
+		
+		// update test
+//		System.out.println("수정 전 : " + memberMapper.findMemberById("mybatis"));
+//		Member paramVo = new Member("mybatis", "b", "황희찬", "올버햄튼");
+//		// id가 mybais인 회원의 password와 name, address를 수정
+//		int updateResult = memberMapper.updateMember(paramVo);
+//		System.out.println("회원 정보 수정 : " + updateResult);
+//		System.out.println("수정 후 : " + memberMapper.findMemberById("mybatis"));
+		
+		// delete test
+		String id = "mybatis";
+		System.out.println(memberMapper.findMemberById(id));	// 황희찬 회원 조회됨
+		int deleteResult = memberMapper.deleteMember(id);
+		System.out.println("삭제 : " + deleteResult);
+		System.out.println(memberMapper.findMemberById(id));	// null
+		
 	}
 
 }
--- spring14-Mybatis-Basic/src/main/java/org/kosa/myproject/mapper/MemberMapper.java
+++ spring14-Mybatis-Basic/src/main/java/org/kosa/myproject/mapper/MemberMapper.java
@@ -1,8 +1,16 @@
 package org.kosa.myproject.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.kosa.myproject.domain.Member;
 
 @Mapper
 public interface MemberMapper {
 	int getTotalMemberCount(); 
+	int register(Member member);
+	Member findMemberById(String id);
+	List<Member> findAllMemberList();
+	int updateMember(Member paramVo);
+	int deleteMember(String id);
 }
