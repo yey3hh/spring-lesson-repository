4417d086ba0b753a623b25ea7aed607f283efd97
yey3hh <wateveryehwant@gmail.com>
2025-07-21 21:56:44 +0900

    Revert "feat: MyBatis ResultMap"
    This reverts commit b4e93ebf5e9fd595c69d1beedfc05c8738d9f845.

--- spring15-MyBatis-product/src/main/java/org/kosa/myproject/Runner/MyBatisProductDynamicStudyRunner.java
+++ spring15-MyBatis-product/src/main/java/org/kosa/myproject/Runner/MyBatisProductDynamicStudyRunner.java
@@ -1,7 +1,9 @@
 package org.kosa.myproject.Runner;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.kosa.myproject.domain.Product;
 import org.kosa.myproject.mapper.ProductMapper;
